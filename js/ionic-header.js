100ecf65eca2b9c5576948266e2ce07a707b0226
Michał Gołębiowski-Owczarek <m.goleb@gmail.com>
2021-07-15 23:11:59 +0200

    Some build stuff + AngularJS 1.5.8

---
+++ js/ionic-header.js
@@ -0,0 +1,12 @@
+/*!
+ * ionic.bundle.js is a concatenation of:
+ * ionic-header.js, ionic.js, angular.js, angular-animate.js,
+ * angular-sanitize.js, angular-ui-router.js,
+ * and ionic-angular.js
+ *
+ * Command to generate:
+ * ```
+ * cat js/ionic-header.js js/ionic.js bower_components/angular/angular.js bower_components/angular-animate/angular-animate.js bower_components/angular-sanitize/angular-sanitize.js bower_components/angular-ui-router/release/angular-ui-router.js js/ionic-angular.js >js/ionic.bundle.js && npx uglify-js js/ionic.bundle.js --compress --mangle --comments '/^!/' >js/ionic.bundle.min.js && git add js/ionic.bundle.{,min.}js
+ * ```
+ */
+
